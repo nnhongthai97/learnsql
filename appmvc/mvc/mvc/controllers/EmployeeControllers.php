db3c75de40d226f600480b1a76b17285239ea1ed
nnhongthai97 <nnhongthai97@gmail.com>
2019-06-21 20:29:02 +0700

    App mvc

--- appmvc/mvc/mvc/controllers/EmployeeControllers.php
+++ appmvc/mvc/mvc/controllers/EmployeeControllers.php
@@ -6,11 +6,60 @@ class EmployeeController {
      * Dùng để điều hướng cho phần hiển thị tất cả các nhận viên
      */
     public function index() {
+        $model = new EmployeeModel();
+        $resul = $model->getAll();
+
+        // trả về view cho phần hiển thị
+        include_once "mvc/views/index.php";
     }
     public function create() {
+        if (isset($_POST) && !empty($_POST)) {
+            $model = new EmployeeModel();
+            $result = $model->insert($_POST);
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+            echo "<br> Lưu dữ liệu không thành công";
+        }
+
+        // trả về view cho phần hiển thị;
+
+        include_once "mvc/views/create.php";
     }
     public function edit() {
+        if (isset($_POST) && !empty($_POST)) {
+            $model = new EmployeeModel();
+            $result = $model->update($_POST);
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+            echo "<br> Cập nhật dữ liệu không thành công";
+        }
+        if (isset($_GET["id"])) {
+            $model = new EmployeeModel();
+            $row = $model->getRow($_GET["id"]);
+            // trả về view cho phần hiển thị
+            include_once "mvc/views/edit.php";
+        }
+        echo "URL không hợp lệ";
+        exit;
+        // trả về view cho phần hiển thị
+        include_once "mvc/views/edit.php";
     }
     public function delete() {
+        if (isset($_POST) && !empty($_POST["id"])) {
+            $model = new EmployeeModel();
+            $result = $model->delete($_POST["id"]);
+            if ($result == true) {
+                header("Location: index.php?controller=employee&action=index");
+                exit;
+            }
+            echo "<br> Xóa dữ liệu không thành công";
+        }
+        // trả về view cho phần hiển thị
+            // trả về view cho phần hiển thị
+        include_once "mvc/views/delete.php";
     }
 }
